33ec1256d9d965e2d23ad645f346a26624490c37
aematei <alexanderematei@gmail.com>
2025-02-21 18:59:22 -0800

    Clean up React boilerplate
    Signed-off-by: aematei <alexanderematei@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import { UserContext } from './UserContext';
 
 function App() {
-  const [count, setCount] = useState(0);
   const userContext = useContext(UserContext);
   if (!userContext) {
     return <div>Error: UserContext is not provided</div>;
@@ -34,26 +31,6 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <div>
         {user ? (
           <div>
