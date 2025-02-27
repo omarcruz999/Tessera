ff52e69f878a271e130223cc341cc2be44a12e58
Jihnboii <rodriguezjohn910@gmail.com>
2025-02-27 12:05:42 -0800

    Profile View tweaks

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,7 +2,7 @@ import ProfileView from "../components/ProfileView";
 
 function Profile() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+    <div>
       <ProfileView />
     </div>
   );
