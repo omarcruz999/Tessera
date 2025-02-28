400408851f313eaf4a570f239233223864f2d293
edgaro9 <edgaro@cpp.edu>
2025-02-28 13:23:51 -0800

    Adding tailwind css

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import logo from 'Public/Tessera.svg'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+        <div>
+            <a>
+                <img src ={logo} alt ="logo"/>
+            </a>
+        </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
