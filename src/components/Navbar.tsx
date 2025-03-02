73141829b2d88ffc61a60e197227eb9873638054
edgaro9 <edgaro@cpp.edu>
2025-03-02 01:28:58 -0800

    Tailwind use in navbar for components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,21 +3,22 @@ import logoimage from '../assets/tesseraLogo.svg'
 import profile from '../assets/Profile Button.svg'
 import home from '../assets/Home Button.svg'
 import dm from '../assets/Direct Messages Button.svg'
-import pic from '../assets/Avatar.svg'
-import search from '../assets/Search Button.svg'     
+
+    
 
 const Navbar = () => {
   return (
-   <nav className='flex w-100% justify-between'>
-    <div>
+   <nav className='flex w-100% justify-between '>
+    <div className='flex items-center flex border border-black p-1 h-20'>
+        <a >
+            <img  src = {logoimage} alt="Tessera " className=' '/>
+        </a>
         <a>
             <img src ={logo} alt="Tessera Logo" />
         </a>
-        <a>
-            <img src = {logoimage} alt="Tessera " />
-        </a>
+      
 </div>
-        <ul>
+        <ul className='flex items-center  border border-black '>
             <li> <a> <img src = {profile} /></a></li>
             <li><a> <img src = {home} /></a> </li>
             <li> <a> <img src = {dm} /></a></li>
