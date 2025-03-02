efba8aa4c3f787ea6236ac017c2a847cb523b4a5
edgaro9 <edgaro@cpp.edu>
2025-03-02 02:32:21 -0800

    Fixed the reason for offset of navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,27 +8,33 @@ import dm from '../assets/Direct Messages Button.svg'
 
 const Navbar = () => {
   return (
-   <nav className='flex w-100% justify-between '>
-    <div className='flex items-center flex border border-black p-1 h-20'>
+    <div className='m-0 p-0 h-20'>
+   <nav className='flex w-full justify-between items-center   '>
+    <div className='flex space-x-4 '>
         <a >
-            <img  src = {logoimage} alt="Tessera " className=' '/>
+            <img  src = {logoimage} alt="Tessera " className=' h-10  '/>
         </a>
-        <a>
-            <img src ={logo} alt="Tessera Logo" />
+        <a className='border border-black '>
+            <img src ={logo} alt="Tessera Logo" className=' h-10 w-30 ' />
         </a>
       
 </div>
-        <ul className='flex items-center  border border-black '>
-            <li> <a> <img src = {profile} /></a></li>
-            <li><a> <img src = {home} /></a> </li>
-            <li> <a> <img src = {dm} /></a></li>
+{/* space-x-4 */}
+        <ul className='flex text-align-center space-x-10 mr-64'>
+            <li className=''> <a> <img src = {profile} className=' h-15' /></a></li>
+            <li><a> <img src = {home}className='h-15' /></a> </li>
+            <li> <a> <img src = {dm} className='h-15'/></a></li>
         </ul>
 
         <a>
             <img src = {profile} alt="Avatar" />
         </a>
    </nav>
+   </div>
   );
 };
+
+
+
 
 export default Navbar
