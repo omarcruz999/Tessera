223ea53b9362ba87b8fb3b742ccf605b24802001
Jihnboii <rodriguezjohn910@gmail.com>
2025-03-12 20:55:50 -0700

    Polished Chat view, added functionality

--- src/components/MessagePreview.tsx
+++ src/components/MessagePreview.tsx
@@ -1,21 +1,37 @@
 const MessagePreview = () => {
-    const handleClick = () => {
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        const messagePreviews = document.querySelectorAll('.message-preview');
+        const target = event.currentTarget as HTMLElement;
         const container = document.querySelector('.messages-container');
         const chatContainer = document.querySelector('.chat-container') as HTMLElement;
-        if (container) {
-            container.classList.toggle('shrink');
-        }
-        if (chatContainer) {
-            if (container?.classList.contains('shrink')) {
-                chatContainer.style.display = 'flex';
-                setTimeout(() => {
-                    chatContainer.style.opacity = '1';
-                }, 10);
-            } else {
+
+        if (target.classList.contains('active')) {
+            // If the clicked message preview is already active, remove all highlights and hide chat container
+            messagePreviews.forEach(preview => preview.classList.remove('active'));
+            if (container) {
+                container.classList.remove('shrink');
+            }
+            if (chatContainer) {
                 chatContainer.style.opacity = '0';
                 setTimeout(() => {
                     chatContainer.style.display = 'none';
                 }, 300); // Match the transition duration
+            }
+        } else {
+            // Remove active class from all message previews
+            messagePreviews.forEach(preview => preview.classList.remove('active'));
+
+            // Add active class to the clicked message preview
+            target.classList.add('active');
+
+            if (container && !container.classList.contains('shrink')) {
+                container.classList.add('shrink');
+                if (chatContainer) {
+                    chatContainer.style.display = 'flex';
+                    setTimeout(() => {
+                        chatContainer.style.opacity = '1';
+                    }, 10);
+                }
             }
         }
     };
