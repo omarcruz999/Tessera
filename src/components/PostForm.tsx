02265108f7aa23fae40e56c1f6a6ab75b6a0fef2
Omar Cruz <ocruzcpp@gmail.com>
2025-03-21 21:51:47 -0700

    Fixed formatting issues

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -20,24 +20,19 @@ function PostForm() {
                 </button>
             </div>
 
-
-
-
-
-
             {/* The mainContentDiv contains the users profile picture and the text area */}
             <div id="mainContentDiv" className="w-full grid grid-cols-[100px_450px] items-start gap-8">
+                
                 {/* The profilePictureDiv contains the users profile picture */}
                 <div id="profilePictureDiv" className="!w-[100px] !h-full rounded-full pl-5">
                     <img src={profilePiture} className="!w-80px !h-85px rounded-full"></img>
                 </div>
-                
+
                 {/* The textarea contains the text the user inputs */}
                 <textarea
                     maxLength={280}
                     className="w-full h-full text-black border-0 outline-none focus:outline-none focus:ring-0 resize-none "
-                    placeholder="What's on your mind?"
-                />
+                    placeholder="What's on your mind?" />
             </div>
 
             {/* This is the line that seperate the mainContentDiv and the optionsDiv */}
@@ -45,32 +40,32 @@ function PostForm() {
 
             {/* The optionsDiv contains the buttons for uploading an image, the toggle button, and post button*/}
             <div id="optionsDiv" className="w-full h-[50px] flex items-center space-x-3 px-2">
+
+                {/* The uploadImageButton contains the button for uploading an image */}
                 <button
+                    id="uploadImageButton"
                     type="button"
-                    className="!w-10 !h-10 !bg-[#FDF7F4] !p-0 !border-0 !focus:outline-none"
-                >
+                    className="!w-10 !h-10 !bg-[#FDF7F4] !p-0 !border-0 !focus:outline-none">
                     <img src={imageIcon} alt="Upload Image" className="w-full h-full object-cover" />
                 </button>
 
-
+                {/* The toggle button for sharing */}
                 <p className="text-black">Allow sharing:</p>
                 <label htmlFor="toggle" className="relative inline-flex items-center cursor-pointer">
                     <input id="toggle" type="checkbox" className="sr-only peer" />
-                    <div className="w-10 h-6 rounded-full bg-gray-200 peer-checked:bg-[#8EB486] transition-colors"> 
-
-                    </div>
+                    <div className="w-10 h-6 rounded-full bg-gray-200 peer-checked:bg-[#8EB486] transition-colors" />
                     <div className="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition-transform peer-checked:translate-x-4" />
                 </label>
 
+                {/* The post button for posting the content */}
                 <button
+                    id="postButton"
                     type="button"
                     className="ml-auto px-4 py-2 !bg-[#8EB486] text-white rounded-lg focus:outline-none"
                 >
                     Post
                 </button>
             </div>
-
-
         </div>
     )
 };
