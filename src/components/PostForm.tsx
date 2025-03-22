d4ecd8a1369f87232889d12e0d41914ae0629fcf
Omar Cruz <ocruzcpp@gmail.com>
2025-03-21 22:32:56 -0700

    Implemented Modal Functionality

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -1,73 +1,61 @@
-import cancelIcon from "../assets/cancelPost.svg"
-import profilePiture from "/public/JohnPork.png"
-import imageIcon from "../assets/imageIcon.svg"
-
-function PostForm() {
+import React from 'react';
+import imageIcon from '../assets/imageIcon.svg';
 
 
+interface PostFormProps {
+    onClose: () => void;
+}
 
-
+const PostForm: React.FC<PostFormProps> = ({ onClose }) => {
     return (
-
-        // Post Form Div
-        <div id="postFormDiv" className="bg-[#FDF7F4] w-[600px] grid grid-rows-[auto_1fr_0.5_auto]">
-
-            {/* The cancelDiv contains the button the user clicks to exit out of the modal */}
-            <div id="cancelDiv" className="w-full h-[50px] flex items-center justify-end pr-3" >
-                <button type="button"
-                    className="!w-8 !h-8 !bg-[#FDF7F4] !rounded-full !p-0 !border-0 !focus:outline-none">
-                    <img src={cancelIcon} alt="Cancel" className="!w-full !h-full !object-cover" />
-                </button>
-            </div>
-
-            {/* The mainContentDiv contains the users profile picture and the text area */}
-            <div id="mainContentDiv" className="w-full grid grid-cols-[100px_450px] items-start gap-8">
-                
-                {/* The profilePictureDiv contains the users profile picture */}
-                <div id="profilePictureDiv" className="!w-[100px] !h-full rounded-full pl-5">
-                    <img src={profilePiture} className="!w-80px !h-85px rounded-full"></img>
+        <div className="!fixed !inset-0 !z-50 !flex !items-center !justify-center !bg-black/40 ">
+            <div
+                id="postFormDiv"
+                className="!bg-[#FDF7F4] !max-w-[600px] !w-full !mx-4 !grid !grid-rows-[auto_1fr_0.5_auto] !rounded-lg !shadow-lg !max-h-[70vh] !overflow-auto"
+            >
+                <div id="cancelDiv" className="!flex !justify-end !p-3">
+                    <button type="button" onClick={onClose} className="!w-10 !h-10 !bg-[#FDF7F4] !rounded-full !p-2 !focus:outline-none">
+                        <img src="src/assets/cancelPost.svg" alt="Cancel" className="!w-full !h-full !object-contain" />
+                    </button>
                 </div>
 
-                {/* The textarea contains the text the user inputs */}
-                <textarea
-                    maxLength={280}
-                    className="w-full h-full text-black border-0 outline-none focus:outline-none focus:ring-0 resize-none "
-                    placeholder="What's on your mind?" />
-            </div>
+                <div id="mainContentDiv" className="!grid !grid-cols-[100px_1fr] !items-start !gap-8 !px-5">
+                    <img src="/JohnPork.png" alt="Profile" className="!w-[80px] !h-[85px] !rounded-full" />
+                    <textarea
+                        maxLength={280}
+                        className="!w-full !h-full !text-black !border-0 !outline-none !resize-none"
+                        placeholder="What's on your mind?"
+                    />
+                </div>
 
-            {/* This is the line that seperate the mainContentDiv and the optionsDiv */}
-            <hr className="border-t border-gray-300 mt-3" />
+                <hr className="!border-t !border-gray-300 !mt-3" />
 
-            {/* The optionsDiv contains the buttons for uploading an image, the toggle button, and post button*/}
-            <div id="optionsDiv" className="w-full h-[50px] flex items-center space-x-3 px-2">
+                <div id="optionsDiv" className="!flex !items-center !space-x-3 !px-5 !py-2">
+                    <button type="button" className="!w-10 !h-10 !p-0 !bg-[#FDF7F4] !focus:outline-none">
+                        <img src={imageIcon} alt="Upload Image" className="!w-full !h-full !object-contain" />
+                    </button>
 
-                {/* The uploadImageButton contains the button for uploading an image */}
-                <button
-                    id="uploadImageButton"
-                    type="button"
-                    className="!w-10 !h-10 !bg-[#FDF7F4] !p-0 !border-0 !focus:outline-none">
-                    <img src={imageIcon} alt="Upload Image" className="w-full h-full object-cover" />
-                </button>
+                    <p className="!text-black">Allow sharing:</p>
+                    <label className="relative inline-flex items-center cursor-pointer">
+                        <input type="checkbox" className="sr-only peer" />
 
-                {/* The toggle button for sharing */}
-                <p className="text-black">Allow sharing:</p>
-                <label htmlFor="toggle" className="relative inline-flex items-center cursor-pointer">
-                    <input id="toggle" type="checkbox" className="sr-only peer" />
-                    <div className="w-10 h-6 rounded-full bg-gray-200 peer-checked:bg-[#8EB486] transition-colors" />
-                    <div className="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition-transform peer-checked:translate-x-4" />
-                </label>
+                        {/* Track */}
+                        <div className="w-10 h-6 bg-gray-200 rounded-full peer-checked:bg-[#8EB486] transition-colors" />
 
-                {/* The post button for posting the content */}
-                <button
-                    id="postButton"
-                    type="button"
-                    className="ml-auto px-4 py-2 !bg-[#8EB486] text-white rounded-lg focus:outline-none"
-                >
-                    Post
-                </button>
+                        {/* Knob */}
+                        <span className="absolute left-1 top-1 bg-white w-4 h-4 rounded-full transition-transform peer-checked:translate-x-4" />
+                    </label>
+
+
+
+
+                    <button type="button" className="!ml-auto !px-4 !py-2 !bg-[#8EB486] !text-white !rounded-lg !focus:outline-none">
+                        Post
+                    </button>
+                </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default PostForm;
