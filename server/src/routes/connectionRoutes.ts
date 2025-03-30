72488e6130c18337ca1890610082402f0f379c9d
aematei <alexanderematei@gmail.com>
2025-03-29 21:53:54 -0700

    Implement connection endpoints

--- server/src/routes/connectionRoutes.ts
+++ server/src/routes/connectionRoutes.ts
@@ -2,29 +2,21 @@
     This file defines the endpoints related to user connections
 */
 
-import { Router } from 'express';
-
+import express from 'express';
 import {
     createConnection,
     deleteConnection,
     updateConnection,
     getConnection,
-    getConnections
+    getConnections,
 } from '../controllers/connectionController';
 
-export const connectionRoutes = Router();
+const router = express.Router();
 
-// Create a new connection
-connectionRoutes.post('/', createConnection);
+router.post('/', createConnection);
+router.delete('/', deleteConnection);
+router.put('/', updateConnection);
+router.get('/', getConnection);
+router.get('/all', getConnections);
 
-// Delete an existing connection by ID
-connectionRoutes.delete('/:id', deleteConnection);
-
-// Update an existing connection by ID
-connectionRoutes.put('/:id', updateConnection);
-
-// Get a specific connection by ID
-connectionRoutes.get('/:id', getConnection);
-
-// Get all connections for a user
-connectionRoutes.get('/', getConnections);
+export default router;
